e93f7e9016529401b9f6ced53bd248e9e3480d3b
Valentino Alexputra <grimmyjames3@gmail.com>
2026-06-26 15:31:49 +0700

    Implement movement update for Slime enemy
    Added movement update logic for Slime enemy.

--- game/src/entities/Slime.java
+++ game/src/entities/Slime.java
@@ -7,5 +7,52 @@ public class Slime extends Enemy{
         super(x, y, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY);
         initHitbox(x, y,(int)(22 * Game.scale), (int)(19 * Game.scale));
     }
+
+    private void updateMove(int[][] lvlData) {
+		if(firstUpdate){
+			if(!IsEntityOnFloor(hitbox, lvlData))
+				inAir = true;
+			firstUpdate = false;
+		}
+
+		if(inAir){
+			if(CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)){
+				hitbox.y += fallSpeed;
+				fallSpeed += gravity;
+			}else{  
+				inAir = false;
+				hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
+			}
+		}else{
+			switch(enemyState){
+				case IDLE:
+					enemyState = RUNNING;
+					break;
+				case RUNNING:
+					float xSpeed = 0;
+
+					if(walkDir == LEFT){
+						xSpeed = -walkSpeed;
+					}else{
+						xSpeed = walkSpeed;
+					}
+
+					if(CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)){
+						if(IsFloor(hitbox, xSpeed, lvlData)){
+							hitbox.x += xSpeed;
+							return;
+						}else{
+							if(walkDir == LEFT){
+								walkDir = RIGHT;
+							}else{
+								walkDir = LEFT;
+							}
+						}
+					}
+					changeWalkDir();
+					break;
+			}
+		}
+	}
     
 }
