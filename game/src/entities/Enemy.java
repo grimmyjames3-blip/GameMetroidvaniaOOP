a9d944074b4686c0a4346f7c9ef30926226f9aa3
Valentino Alexputra <grimmyjames3@gmail.com>
2026-06-26 15:31:30 +0700

    Refactor Enemy class for improved readability

--- game/src/entities/Enemy.java
+++ game/src/entities/Enemy.java
@@ -36,52 +36,7 @@ public abstract class Enemy extends Entity{
 		updateAnimationTick();
 	}
 
-	private void updateMove(int[][] lvlData) {
-		if(firstUpdate){
-			if(!IsEntityOnFloor(hitbox, lvlData))
-				inAir = true;
-			firstUpdate = false;
-		}
-
-		if(inAir){
-			if(CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)){
-				hitbox.y += fallSpeed;
-				fallSpeed += gravity;
-			}else{  
-				inAir = false;
-				hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed);
-			}
-		}else{
-			switch(enemyState){
-				case IDLE:
-					enemyState = RUNNING;
-					break;
-				case RUNNING:
-					float xSpeed = 0;
-
-					if(walkDir == LEFT){
-						xSpeed = -walkSpeed;
-					}else{
-						xSpeed = walkSpeed;
-					}
-
-					if(CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)){
-						if(IsFloor(hitbox, xSpeed, lvlData)){
-							hitbox.x += xSpeed;
-							return;
-						}else{
-							if(walkDir == LEFT){
-								walkDir = RIGHT;
-							}else{
-								walkDir = LEFT;
-							}
-						}
-					}
-					changeWalkDir();
-					break;
-			}
-		}
-	}
+	
 
 	private void changeWalkDir() {
 		if(walkDir == LEFT){
