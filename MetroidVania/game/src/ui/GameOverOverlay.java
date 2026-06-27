2ae7b803fa145a31f56e25867f698b3fda096e85
Valentino Alexputra <grimmyjames3@gmail.com>
2026-06-28 00:36:31 +0700

    Enhance GameOverOverlay with buttons and image
    Refactored GameOverOverlay to include buttons and image.

--- MetroidVania/game/src/ui/GameOverOverlay.java
+++ MetroidVania/game/src/ui/GameOverOverlay.java
@@ -1,27 +1,65 @@
 package ui;
 
+import static utils.Constants.UI.URMButtons.URM_SIZE;
+
 import gamestates.Gamestate;
 import gamestates.Playing;
 import java.awt.Color;
 import java.awt.Graphics;   
 import java.awt.event.KeyEvent;
-import main.Game;             
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import main.Game;    
+import utils.LoadSave;         
+
 
 public class GameOverOverlay {
 
 	private Playing playing;
+	private BufferedImage img;
+	private int imgX, imgY, imgW, imgH;
+	private UrmButton menu, play;
 
 	public GameOverOverlay(Playing playing) {
 		this.playing = playing;
+		createImg();
+		createButtons();
+	}
+
+	private void createButtons() {
+		int menuX = (int) (335 * Game.SCALE);
+		int playX = (int) (440 * Game.SCALE);
+		int y = (int) (195 * Game.SCALE);
+		play = new UrmButton(playX, y, URM_SIZE, URM_SIZE, 0);
+		menu = new UrmButton(menuX, y, URM_SIZE, URM_SIZE, 2);
+	}
+
+	private void createImg() {
+		img = LoadSave.GetSpriteAtlas(LoadSave.DEATH_SCREEN);
+		imgX = (int) (img.getWidth() * Game.SCALE);
+		imgY = (int) (img.getHeight() * Game.SCALE);
+		imgW = Game.GAME_WIDTH / 2 - imgW / 2;
+		imgH = (int) (100 * Game.SCALE);
 	}
 
 	public void draw(Graphics g) {
 		g.setColor(new Color(0, 0, 0, 200));
 		g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT); 
 
-		g.setColor(Color.WHITE);
-		g.drawString("Game Over", Game.GAME_WIDTH / 2, 150);
-		g.drawString("Press ESC to return to main menu", Game.GAME_WIDTH / 2, 300);
+		g.drawImage(img, imgX, imgY, imgW, imgH, null);
+
+		menu.draw(g);
+		play.draw(g);
+
+		// g.setColor(Color.WHITE);
+		// g.drawString("Game Over", Game.GAME_WIDTH / 2, 150);
+		// g.drawString("Press ESC to return to main menu", Game.GAME_WIDTH / 2, 300);
+	}
+
+	public void update() {
+		menu.update();
+		play.update();
 	}
 
 	public void keyPressed(KeyEvent e) { 
@@ -29,5 +67,40 @@ public class GameOverOverlay {
 			playing.resetAll();
 			Gamestate.state = Gamestate.MENU; 
 		}
+	}
+
+	private boolean isIn(UrmButton b, MouseEvent e) {
+		return b.getBounds().contains(e.getX(), e.getY());
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		play.setMouseOver(false);
+		menu.setMouseOver(false);
+		if (isIn(play, e))
+			play.setMouseOver(true);
+		else if (isIn(menu, e))
+			menu.setMouseOver(true);
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		if (isIn(menu, e)) {
+			if (menu.isMousePressed()){
+				playing.resetAll();
+				Gamestate.state = Gamestate.MENU;
+			}
+		} else if (isIn(play, e)) {
+			if (play.isMousePressed()){
+				playing.resetAll();
+			}
+		}
+		play.resetBools();
+		menu.resetBools();
+	}
+
+	public void mousePressed(MouseEvent e) {
+		if (isIn(play, e))
+			play.setMousePressed(true);
+		else if (isIn(menu, e))
+			menu.setMousePressed(true);
 	}
 }
